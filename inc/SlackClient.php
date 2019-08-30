6c4113fad1a074e140b8b12169e66bdefd249dd6
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-08-30 04:27:51 +0000

    add slackclient test

--- inc/SlackClient.php
+++ inc/SlackClient.php
@@ -22,6 +22,6 @@ class SlackClient
                 "channel" => $this->channel,
                 "text" => $text
             )
-        ))
+        ));
     }
 }
--- tests/SlackClientTest.php
+++ tests/SlackClientTest.php
@@ -2,17 +2,38 @@
 declare(strict_types=1);
 
 use StanfordDaily\Tipster\SlackClient;
+use StanfordDaily\Tipster\HTTPClient;
 use PHPUnit\Framework\TestCase;
 
 final class SlackClientTest extends TestCase
 {
 
-    public function testInit(): void
+    public function testSendMessage(): void
     {
-        $client = SlackClient(array(
+
+        $expectedCall = array(
+            "authorization" => "token",
+            "url" => "https://slack.com/api/chat.postMessage",
+            "data" => array(
+                "channel" => "channel",
+                "text" => "text"
+            )
+        );
+
+        $clientMock = $this->getMockBuilder(HTTPClient::class)
+                         ->setMethods(['post'])
+                         ->getMock();
+
+        $clientMock->expects($this->once())
+            ->method('update')
+            ->with($this->equalTo($expectedCall));
+
+        $client = new SlackClient(array(
             "token" => "token",
             "channel" => "channel"
         ));
+
+        $client->send_message("Test message");
     }
 
 }
